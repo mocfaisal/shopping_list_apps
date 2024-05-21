8fc039b810b3b08b20218623a5cf9beb4bc086d3
Salz <faisal_animator@yahoo.com>
2024-05-21 20:57:35 +0700

    feat: Add Step 4

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
 import '/data/dummy_items.dart';
+import '/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
