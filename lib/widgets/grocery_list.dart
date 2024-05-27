107aa2e7ea961af3f055d973f600d6ed3c3b99d0
Salz <faisal_animator@yahoo.com>
2024-05-27 14:17:18 +0700

    feat: Add Step 4

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -26,16 +26,17 @@ class _GroceryListState extends State<GroceryList> {
 
   void _loadItems() async {
     final url = Uri.https(
-        'shopping-list-apps-d1182-default-rtdb.asia-southeast1.firebasedatabase.app', 'shopping-list.json');
+        'shopping-list-apps-d1182-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'shopping-list.json');
     final response = await http.get(url);
     final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> _loadedItems = [];
+    final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
       final category = categories.entries
           .firstWhere(
               (catItem) => catItem.value.title == item.value['category'])
           .value;
-      _loadedItems.add(
+      loadedItems.add(
         GroceryItem(
           id: item.key,
           name: item.value['name'],
@@ -45,18 +46,24 @@ class _GroceryListState extends State<GroceryList> {
       );
     }
     setState(() {
-      _groceryItems = _loadedItems;
+      _groceryItems = loadedItems;
     });
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    _loadItems();
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   void _removeItem(GroceryItem item) {
